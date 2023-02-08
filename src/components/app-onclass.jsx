ccd6386f06093288e4fd9da725a6482e2286126d
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-08 15:14:45 +0200

    comit

--- src/components/app-onclass.jsx
+++ src/components/app-onclass.jsx
@@ -1,9 +1,9 @@
 
-import { Component } from 'react';
-import css from './App.module.css';
-import ContactForm from './ContactsForm/ContactsForm';
-import Filter from './Filter/Filter';
-import ContactList from './ContactsList/ContactsList';
+// import { Component } from 'react';
+// import css from './App.module.css';
+// import ContactForm from './ContactsForm/ContactsForm';
+// import Filter from './Filter/Filter';
+// import ContactList from './ContactsList/ContactsList';
 
 // export class App extends Component {
 //   state = {
@@ -33,31 +33,31 @@ import ContactList from './ContactsList/ContactsList';
 //     }));
 //   };
 
-  filter = data => {
-    this.setState({ filter: data.currentTarget.value });
-  };
+//   filter = data => {
+//     this.setState({ filter: data.currentTarget.value });
+//   };
 
-  addContact = ({ name, number, id }) => {
-    this.state.contacts.some(e => e.name === name)
-      ? alert(`${name} is already in contacts`)
-      : this.setState(prevState => ({
-          contacts: [{ name, number, id }, ...prevState.contacts],
-        }));
-  };
+//   addContact = ({ name, number, id }) => {
+//     this.state.contacts.some(e => e.name === name)
+//       ? alert(`${name} is already in contacts`)
+//       : this.setState(prevState => ({
+//           contacts: [{ name, number, id }, ...prevState.contacts],
+//         }));
+//   };
 
-  render() {
-    return (
-      <div className={css.form}>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.filter} />
-        <ContactList
-          contacts={this.filteredContacts()}
-          onClick={this.deleteItem}
-        />
-      </div>
-    );
-  }
-}
-export default App;
+//   render() {
+//     return (
+//       <div className={css.form}>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.addContact} />
+//         <h2>Contacts</h2>
+//         <Filter value={this.state.filter} onChange={this.filter} />
+//         <ContactList
+//           contacts={this.filteredContacts()}
+//           onClick={this.deleteItem}
+//         />
+//       </div>
+//     );
+//   }
+// }
+// export default App;
